d2673cb0041c2d6379976e94effd79cf140b218b
alexei_upornikov <alexei.upornikov@karsa.fi>
2021-07-28 19:10:12 +0300

    Added forwarding of BaseTestClient target exception

--- karsa-backend/test/system/test_clients.py
+++ karsa-backend/test/system/test_clients.py
@@ -7,23 +7,29 @@ import threading
 import time
 import asynctest
 
-from systestlib import start_client_as_daemon
+from systestlib import start_test_client_as_daemon
 
 
 class TestBaseTestClient(asynctest.TestCase):
     def setUp(self) -> None:
-        self.client = start_client_as_daemon()
+        self.client = start_test_client_as_daemon()
         return super().setUp()
 
     def tearDown(self) -> None:
-        asyncio.run(self.client.join_requests())
-        self.client.stop_client('unittest tearDown')
+        # asyncio.run(self.client.join_requests())
+        self.client.stop_client(f'{self.__class__.__name__} tearDown')
         return super().tearDown()
+
+    def verify_client_status(self):
+        asyncio.run(self.client.join_requests())
+        if self.client.target_exception:
+            self.fail(str(self.client.target_exception))
 
     def test_visualize_full_range(self):
         fname = 'TofDaq_Data_2021.07.23_02h13m40s'
         rq_suffix = self.client.set_test_params(fname)
         asyncio.run(self.client.emit_visualize_range(fname, request_id=f"fullrange_{rq_suffix}"))
+        self.verify_client_status()
 
     # def test_visualize_zoomed_range(self):
     #     # TODO: fix handlers for a separate use and together with visualize_full_range
