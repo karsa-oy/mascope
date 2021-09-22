03ead175a5350108ac1d276e5326387e26cfda8e
Oskari Kausiala <oskari.kausiala@karsa.fi>
2021-09-22 17:06:44 +0300

    Added methods to mass cailbrate batch of TOF data files. Need to be reviewed and refactored

--- karsa-backend/src/services/services/SignalProcessorService.py
+++ karsa-backend/src/services/services/SignalProcessorService.py
@@ -12,15 +12,17 @@ from queue import Empty
 
 from scipy.signal import find_peaks
 
+from karsalib.chemistry import match_mz
 from karsalib.client import BaseClientNamespace, BaseServiceClient
 from karsalib.struct import AttrDict, ExtendableDataArray
 from karsalib.util import parse_cmd_args
+from scenthound.karsavlm.msAlign import find_vlm
 from scenthound.kfeeder import KFeeder, FeederProcessor
 from scenthound.kworker import KEncoder
 from scenthound.kcollector import KCollector
 # from scenthond.kpeak import load_peak_dict
 
-from services.FileIoService import filename_to_zarr_path, load_file
+from services.FileIoService import load_file, update_zarr_array_coord
 
 
 
@@ -36,6 +38,7 @@ class SignalProcessorNamespace(BaseClientNamespace):
                 # 'acquisition_coordinates',
                 #  'acquired_spectrum',
                 #  'acquisition_finished',
+                 'mz_calibrate_samples',
                  'peak_data_request',
                  ]
     
@@ -159,6 +162,82 @@ class SignalProcessorNamespace(BaseClientNamespace):
         global feeder
         feeder.queue_in.put(None)
 
+    async def on_mz_calibrate_samples(self, data):
+        self.log(data)
+        value = data['value']
+        filenames = value['filenames']
+        peaklist = value['peaklist']
+        parameters = value.get('parameters', {})
+
+        mz_tolerance = parameters.get('mz_tolerance', 20)
+        min_peak_height = parameters.get('peak_threshold', 1e-2)
+        min_peak_distance = parameters.get('peak_separation', 3)
+        min_peak_width = parameters.get('peak_width', 3)
+
+        # Load samples
+        samples = []
+        peak_lists = []
+        for filename in filenames:
+            # Check if file is cached
+            cache_item = cache.get(filename, None)
+            if not cache_item:
+                # File not in cache, load
+                print("Loading file: %s" %filename)
+                cache_item = load_file(filename, vars=['signal']) # TODO: Load a subset of arrays from file
+                # cache[filename] = cache_item
+                sum_spectrum = cache_item.signal.mean(dim='time').compute()
+
+                peak_ind, peak_props = find_peaks(sum_spectrum,
+                                                  height=min_peak_height,
+                                                  distance=min_peak_distance,
+                                                  width=min_peak_width
+                                                  )
+                peak_mz = sum_spectrum.mz[peak_ind].values.astype(np.float32)
+                samples.append(cache_item)
+                peak_lists.append( (peak_ind, peak_mz) )
+            await asyncio.sleep(0)
+
+        # Find VLM points
+        peak_mz_arrays = list(zip(*peak_lists))[1]
+        vlm_mzs, vlm_per_sample = find_vlm(peak_mz_arrays, mz_tolerance*1e-6, None)
+        print("Found %s virtual lock-masses" %len(vlm_mzs))
+        # Collect VLM peaks of all samples
+        vlm_peak_lists = [ [] for _ in range(len(samples)) ]
+        for vlm_pts_sample in vlm_per_sample:
+            for vlm_pt in vlm_pts_sample:
+                sample_ind, peak_i, peak_mz = vlm_pt
+                peak_ind = peak_lists[sample_ind][0][peak_i]
+                vlm_peak_lists[sample_ind].append( (peak_ind, peak_mz) )
+        # Identify VLM points
+        match_is, match_mzs = zip(*[match_mz(mz, peaklist, tolerance=mz_tolerance)
+                                    for mz in vlm_mzs
+                                    ]
+                                  )
+        # Filter matches
+        mask = np.array( [False] * len(vlm_mzs) )
+        exact_mzs = []
+        exact_mz_i = []
+        for i, vlm_mz_matches in enumerate(match_mzs):
+            if len(vlm_mz_matches) == 1:
+                # Unique match
+                mask[i] = True
+                exact_mzs.append(vlm_mz_matches[0])
+                exact_mz_i.append(match_is[i][0])
+        print("Calibration points: %s" %exact_mzs)
+        # Mass calibrate
+        for i, sample in enumerate(samples):
+            print("Calibrating sample: %s" %sample.filename)
+            vlm_tofs_sample, vlm_mzs_sample = zip(*vlm_peak_lists[i])
+            # Fit mz function and compute new mz coordinates
+            new_mz = mz_calibrate_tof(np.array(vlm_tofs_sample)[mask],
+                                      np.array(vlm_mzs_sample)[mask],
+                                      exact_mzs,
+                                      len(sample.mz)
+                                      )
+            # Write new mz coordinates to file
+            update_zarr_array_coord(sample.filename, 'signal', 'mz', new_mz)
+            await asyncio.sleep(0)
+
     async def on_peak_data_request(self, data):
         self.log(data)
         value = data['value']
@@ -209,6 +288,7 @@ class SignalProcessorNamespace(BaseClientNamespace):
                                             peak_data,
                                             room=client_room
                                             )
+
 
 async def initialize_feeder():
     global feeder
@@ -295,6 +375,43 @@ async def initialize_collector():
     collector = KCollector(results_q)
     collector.start()
 
+def mz_calibrate_tof(peak_tof, peak_mz, exact_mz, nbr_tof_samples):
+    from karsatof.lib.TwTool import TwMassCalibrate, TwTof2Mass
+    # Prepare arguments
+    massCalibMode = 2
+    nbrPoints = len(peak_tof)
+    mass = np.array(exact_mz, dtype=np.double)
+    tof = np.array(peak_tof, dtype=np.double)
+    weight = np.ones((nbrPoints,)) # TODO: Set weights?
+    nbrParams = np.array([3], dtype=np.int)
+    p = np.zeros((nbrParams[0],), dtype=np.double)
+    legacyA = legacyB = np.array([None], dtype=np.double)
+    # Calibrate
+    ret = TwMassCalibrate(massCalibMode,
+                          nbrPoints,
+                          mass,
+                          tof,
+                          weight,
+                          nbrParams,
+                          p,
+                          legacyA,
+                          legacyB
+                          )
+    new_mz_coord = [TwTof2Mass(tof, massCalibMode, p)
+                    for tof in range(nbr_tof_samples)
+                    ]
+
+    new_peak_mz = [ new_mz_coord[p] for p in peak_tof ]
+    pre_dmz = mass - peak_mz
+    post_dmz = mass - new_peak_mz
+    pre_dmz_norm = np.linalg.norm(pre_dmz)
+    post_dmz_norm = np.linalg.norm(post_dmz)
+
+    print("dmz norm pre-calib: %.4f, post_calib: %.4f"
+          %(pre_dmz_norm, post_dmz_norm)
+          )
+
+    return new_mz_coord
 
 
 class SignalProcessorClient(BaseServiceClient):
