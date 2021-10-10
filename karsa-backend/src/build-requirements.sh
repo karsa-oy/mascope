e056337dedf37cd22bc0c8534a526eb7b5768159
Matan Shenhav <matan@fluxcraft.net>
2021-10-11 00:04:17 +0300

    Dockerize Karsa backend and frontend
    This dockerizes the application, making it easy to port to a
    variety of environments.
    
    The main motivation is to setup a development environment for
    myself, but this could be of use for other developers, and
    eventually be also modified for use in CI/CD pipelines, testing
    and deployment.

---
+++ karsa-backend/src/build-requirements.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# External and internal requirement seperation
+# is performed to optimize docker image builds
+
+# This rebuilds `external-requirements.txt`
+# It collects all external dependencies of Karsa's modules
+# and combines them with any `extra-requirements.txt` specified
+
+# pip-compile requires pip-tools and setuptools to run
+pip-compile -o external-requirements.txt ./internal-requirements.txt ./extra-requirements.txt
+
+# This removes Karsa internal dependencies from the generated file
+sed -i '/^-e file/d' external-requirements.txt
+sed -i '/^    # via -r .\/internal-requirements.txt/d' external-requirements.txt
