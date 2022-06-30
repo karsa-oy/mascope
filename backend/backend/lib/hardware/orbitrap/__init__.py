6ad0500596ab38ccd3bd9be660f8b8bb14d411c9
Matan Shenhav <matan.shenhav@karsa.fi>
2022-06-30 15:03:18 +0000

    Fix karsaorbi import error

--- backend/backend/lib/hardware/orbitrap/__init__.py
+++ backend/backend/lib/hardware/orbitrap/__init__.py
@@ -5,3 +5,14 @@ Created on Thu Feb 13 13:11:53 2020
 
 @author: Oskari Kausiala
 """
+import os
+import clr
+
+package_dir = os.path.dirname(os.path.abspath(__file__))
+dll_path = os.path.join(package_dir, "lib", "dlls")
+dlls = [
+    'ThermoFisher.CommonCore.Data.dll'
+]
+for dll in dlls:
+    reference = os.path.join(dll_path, dll)
+    clr.AddReference(reference)
