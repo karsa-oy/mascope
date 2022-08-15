9b71606bcbe75fd66dbee4c8aaea7db80714d067
Oskari Kausiala <oskari.kausiala@karsa.fi>
2022-08-15 09:55:18 +0300

    Load environment variables from .env.development

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -1,7 +1,10 @@
+from dotenv import load_dotenv
 import uvicorn
 import os
 
 #from backend.api import *  # noqa - import ensures api is instantiated
+
+load_dotenv(dotenv_path="../.env.development")
 
 api_port = int(os.environ.get('MASCOPE_PUBLIC_API_PORT'))
 mode = os.environ.get('MASCOPE_PUBLIC_MODE')
