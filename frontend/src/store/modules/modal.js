bfdb476421a1bbf59b21437cd4dbfc408337c691
Oskari Kausiala <oskari.kausiala@karsa.fi>
2022-08-29 13:30:59 +0300

    Use vuex-pathify to generate mutations

--- frontend/src/store/modules/modal.js
+++ frontend/src/store/modules/modal.js
@@ -1,35 +1,41 @@
+import { make } from 'vuex-pathify';
+
+const state = {
+    // active modal indicator
+    active: null,
+    // modal-specific active sync helpers and data
+    targetCollectionOpActive: false,
+    targetCollectionOpProps: {
+
+    },
+    workspaceSaveActive: false,
+    workspaceSaveProps: {
+        // action ('create', 'edit' or 'delete')
+        // workspaceId (required for edit or delete)
+    },
+    sampleBatchOpActive: false,
+    sampleBatchOpProps: {
+        // action ('create', 'edit' or 'delete')
+        // workspaceId (required for edit or delete)
+    },
+    sampleFileAttributesSaveActive: false,
+    sampleFileAttributesSaveProps: {
+        // action ('create', 'edit' or 'delete')
+        // sampleFileId (required for edit or delete)
+    },
+    sampleItemAttributesSaveActive: false,
+    sampleItemAttributesSaveProps: {
+        // action ('create', 'edit' or 'delete')
+        // sampleItemId (required for edit or delete)
+    }
+};
+
 export default {
     namespaced: true,
-    state: {
-        // active modal indicator
-        active: null,
-        // modal-specific active sync helpers and data
-        targetCollectionOpActive: false,
-        targetCollectionOpProps: {
-
-        },
-        workspaceSaveActive: false,
-        workspaceSaveProps: {
-            // action ('create', 'edit' or 'delete')
-            // workspaceId (required for edit or delete)
-        },
-        sampleBatchOpActive: false,
-        sampleBatchOpProps: {
-            // action ('create', 'edit' or 'delete')
-            // workspaceId (required for edit or delete)
-        },
-        sampleFileAttributesSaveActive: false,
-        sampleFileAttributesSaveProps: {
-            // action ('create', 'edit' or 'delete')
-            // sampleFileId (required for edit or delete)
-        },
-        sampleItemAttributesSaveActive: false,
-        sampleItemAttributesSaveProps: {
-            // action ('create', 'edit' or 'delete')
-            // sampleItemId (required for edit or delete)
-        },
-    },
+    state,
     mutations: {
+        ...make.mutations(state),
+
         activate(state, { modal }) {
             state.active = modal;
             state[modal + 'Active'] = true;
@@ -39,24 +45,6 @@ export default {
                 state[activeState] = false;
             }
             state.active = null;
-        },
-        SET_SAMPLE_BATCH_OP_PROPS(state, value) {
-            state.sampleBatchOpProps = value;
-        },
-        SET_SAMPLE_FILE_ATTRIBUTES_SAVE_ACTIVE(state, value) {
-            state.sampleFileAttributesSaveActive = value;
-        },
-        SET_SAMPLE_FILE_ATTRIBUTES_SAVE_PROPS(state, value) {
-            state.sampleFileAttributesSaveProps = value;
-        },
-        SET_SAMPLE_ITEM_ATTRIBUTES_SAVE_ACTIVE(state, value) {
-            state.sampleItemAttributesSaveActive = value;
-        },
-        SET_SAMPLE_ITEM_ATTRIBUTES_SAVE_PROPS(state, value) {
-            state.sampleItemAttributesSaveProps = value;
-        },
-        SET_WORKSPACE_SAVE_PROPS(state, value) {
-            state.workspaceSaveProps = value;
         },
     }
 }
