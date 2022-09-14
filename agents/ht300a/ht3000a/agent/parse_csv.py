1d4d3d9acb18f37d17e5eb22e66cabb56c52b078
Oskari Kausiala <oskari.kausiala@karsa.fi>
2022-09-14 14:46:58 +0300

    Rename and refactor autosampler csv parser a bit

--- agents/ht300a/ht3000a/agent/parse_csv.py
+++ agents/ht300a/ht3000a/agent/parse_csv.py
@@ -1,6 +1,17 @@
 import csv
+import os
+import sys
 
 def parse_csv_report(f):
+    def dup_cycles(rows):
+        res = []
+        for row in rows:
+            cycles = row.get('Cycle(s)', 1)
+            row['Cycle(s)'] = 1
+            for i in range(int(cycles)):
+                res.append(row)
+        return res
+
     res = []
     d = {}
     rows = csv.reader(f)
@@ -22,21 +33,18 @@ def parse_csv_report(f):
             d[k.strip()] = v.strip()
     if 'Sequence step' in d:
         res.append(d)
-    return res
-
-
-def dup_cycles(rows):
-    res = []
-    for row in rows:
-        cycles = row.get('Cycle(s)', 1)
-        row['Cycle(s)'] = 1
-        for i in range(int(cycles)):
-            res.append(row)
-    return res
+    return dup_cycles(res)
 
 
 # example of usage
 if __name__ == "__main__":
-    with open('test.csv') as f:
+    filename = sys.argv[1]
+    # read
+    with open(filename) as f:
         res = parse_csv_report(f)
-        res = dup_cycles(res)
+    # write
+    new_filename = '_'.join(os.path.splitext(filename))
+    with open(new_filename, 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=res[0].keys())
+        writer.writeheader()
+        writer.writerows(res)
