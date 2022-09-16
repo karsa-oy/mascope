8e9004de74118b81a722c21f3609a03959113021
alexei_upornikov <alexei.upornikov@karsa.fi>
2022-09-16 19:38:19 +0300

    Fix production/dev env setups.

--- deploy/dev.win/_deploy.cmd
+++ deploy/dev.win/_deploy.cmd
@@ -6,6 +6,9 @@ echo ** Stopping current version...
 cd %project_path%
 call %mypath%\_kill.cmd
 
+echo Set MASCOPE environment...
+FOR /F "eol=# tokens=*" %%i IN (%~dp0.env) DO SET %%i
+
 echo ** Installing latest version...
 copy /y %mypath%\.env %project_path%\.env
 cd %project_path%
