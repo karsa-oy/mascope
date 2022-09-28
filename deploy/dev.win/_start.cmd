e5f677d70f9f1e7a5beed18f2cf4a6bc07a73a68
Oskari Kausiala <oskari.kausiala@karsa.fi>
2022-09-28 13:11:16 +0300

    Updated dev.win deployment start script to include file converters

--- deploy/dev.win/_start.cmd
+++ deploy/dev.win/_start.cmd
@@ -6,3 +6,13 @@ popd
 pushd frontend
 start "mascope-frontend" cmd /k yarn serve --host
 popd
+
+pushd backend
+start "mascope-backend-file-converter-kltof1" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/kltof1.yaml
+popd
+pushd backend
+start "mascope-backend-file-converter-kltof2" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/kltof2.yaml
+popd
+pushd backend
+start "mascope-backend-file-converter-korbi1" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/korbi1.yaml
+popd
