5674adb3b57ba67aa3207fbd524805f2bf785ac2
Oskari Kausiala <oskari.kausiala@karsa.fi>
2022-11-08 21:16:13 +0200

    Add file downloader entry point and commands to the _start script

--- deploy/dev.win/_start.cmd
+++ deploy/dev.win/_start.cmd
@@ -8,11 +8,21 @@ start "mascope-frontend" cmd /k yarn serve --host
 popd
 
 pushd backend
-start "mascope-backend-file-converter-kltof1" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/kltof1.yaml
+start "mascope-backend-file-converter-kltof1" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/KLTOF1.yaml
 popd
 pushd backend
-start "mascope-backend-file-converter-kltof2" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/kltof2.yaml
+start "mascope-backend-file-converter-kltof2" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/KLTOF2.yaml
 popd
 pushd backend
-start "mascope-backend-file-converter-korbi1" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/korbi1.yaml
+start "mascope-backend-file-converter-korbi1" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/KORBI1.yaml
+popd
+
+pushd backend
+start "mascope-backend-file-downloader-kltof1" cmd /k poetry run file-downloader --config ./backend/service/file_downloader_config/KLTOF1.yaml
+popd
+pushd backend
+start "mascope-backend-file-downloader-kltof2" cmd /k poetry run file-downloader --config ./backend/service/file_downloader_config/KLTOF2.yaml
+popd
+pushd backend
+start "mascope-backend-file-downloader-korbi1" cmd /k poetry run file-downloader --config ./backend/service/file_downloader_config/KORBI1.yaml
 popd
