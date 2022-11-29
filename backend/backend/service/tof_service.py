00fd936f9f58b332f3efb02e82351af180d6a92e
alexei_upornikov <alexei.upornikov@karsa.fi>
2022-11-29 10:27:07 +0200

    Add tof service

---
+++ backend/backend/service/tof_service.py
@@ -0,0 +1,167 @@
+import argparse
+import asyncio
+import os
+import shutil
+import socketio
+
+from multiprocessing import Event
+from queue import Empty
+
+from hardware.tofwerk.tof_streamer import TofDaqStreamer
+from lib.util import load_env_yaml
+
+
+def parse_cmd_args():
+    """
+    Parse command line arguments
+    ------------------------------
+    Return dict
+    Default argument values: see default_args.
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-c", "--config",
+        help="path to yaml config file",
+        type=str, required=False
+    )
+    parser.add_argument(
+        "-m", "--host",
+        help="Mascope host IP",
+        type=str, required=False
+    )
+    parser.add_argument(
+        "-i", "--instrument",
+        help="instrument name",
+        type=str, required=False
+    )
+    parser.add_argument(
+        "-p", "--port",
+        help="Mascope socket.io port",
+        type=str, required=False
+    )
+    parser.add_argument(
+        "-t", "--target",
+        help="target directory",
+        type=str, required=False
+    )
+
+    all_args = parser.parse_args()
+    cmdline_args = {}
+    for arg in vars(all_args):
+        if vars(all_args)[arg] is None:
+            continue
+        cmdline_args[arg] = vars(all_args)[arg]
+    file_args = {}
+    if all_args.config:
+        # service config may be defined in yaml file
+        file_args = load_env_yaml(all_args.config)
+    return {
+        **file_args,
+        **cmdline_args
+        }
+
+
+async def streamer_processor(streamer):
+    global cache
+    global instrument_name
+    # Handlers
+    async def handle_spec_data(data):
+        filename = data['filename']
+        spec_i = data['i']
+        if spec_i is None:
+            # File finished
+            print("File finished")
+            raw_filename = data['source_filepath']
+            global instrument_name
+            global target_path
+            mailbox_path = os.path.join(
+                target_path,
+                instrument_name,
+                os.path.basename(raw_filename)
+                )
+            shutil.copyfile(raw_filename, mailbox_path)
+        elif spec_i < 0:
+            # New file
+            print("New file: %s" %filename)
+        else:
+            # New data to existing file
+            pass
+        print("%.2f" %streamer.progress)
+        return True
+
+    async def handle_tps_data(data):
+        return
+
+        # Main loop
+    while not streamer.shutdown_event.is_set():
+        try:
+            spec_data = streamer.spec_queue.get_nowait()
+            success = await handle_spec_data(spec_data)
+            if success and hasattr(streamer, 'tps_queue'):
+                tps_data = streamer.tps_queue.get()
+                await handle_tps_data(tps_data)
+        except Empty:
+            await asyncio.sleep(.1)
+
+
+async def main():
+    global host
+    global port
+    global sio
+
+    url = f"http://{host}:{port}"
+    while not shutdown_event.is_set():
+        try:
+            print("Connecting to %s" %url)
+            await sio.connect(url)
+            break
+        except:
+            await asyncio.sleep(1)
+    while not shutdown_event.is_set():
+        await asyncio.sleep(1)
+
+
+host = None
+instrument_name = None
+port = None
+shutdown_event = Event()
+sio = socketio.AsyncClient(logger=True)
+target_path = None
+
+def run():
+    global host
+    global instrument_name
+    global port
+    global shutdown_event
+    global target_path
+
+    args = parse_cmd_args()
+
+    host = args.get('host', os.environ['MASCOPE_PUBLIC_HOST'])
+    port = args.get('port', os.environ['MASCOPE_PUBLIC_API_PORT'])
+    instrument_name = args.get('instrument', 'unknown')
+    target_path = args.get(
+        'target',
+        os.environ.get('MASCOPE_PRIVATE_DOWNLOADER_DIR', '.')
+        )
+
+    streamer = TofDaqStreamer(
+        shutdown_event=shutdown_event,
+    )
+    streamer.start()
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(streamer_processor(streamer))
+
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        shutdown_event.set()
+    except Exception as e:
+        print(e)
+        shutdown_event.set()
+
+
+if __name__ == '__main__':
+    run()
