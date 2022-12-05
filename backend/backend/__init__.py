b51b1e43adf61f4a32929f7c3c87dac74b1ce382
alexei_upornikov <alexei.upornikov@karsa.fi>
2022-12-06 00:16:53 +0200

    Fix api server ip for dev.deployment

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -13,7 +13,7 @@ mode = os.environ.get('MASCOPE_PUBLIC_MODE')
 def run():
     uvicorn.run(
         'backend.server:app',
-        host='127.0.0.1',
+        host='0.0.0.0' if mode=='development' else '127.0.0.1',
         port=api_port,
         reload=(mode == 'development')
     )
