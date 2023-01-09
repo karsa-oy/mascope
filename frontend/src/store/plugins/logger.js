3c9cbb509fa61c306e30295711ab17bd126577f8
Oskari Kausiala <oskari.kausiala@karsa.fi>
2023-01-09 14:40:39 +0200

    Reduce frontend logging (disable vuex mutations from log)

--- frontend/src/store/plugins/logger.js
+++ frontend/src/store/plugins/logger.js
@@ -2,15 +2,6 @@ import createLogger from 'vuex/dist/logger'
 
 export default createLogger({
     filter(mutation) {
-        let hiddenMutationTypes = [
-            'setPath',
-            'key/activate',
-            'key/deactivate',
-            'target/SET_SELECTION',
-            'match/LOAD',
-            'match/UNLOAD',
-        ]
-        return !hiddenMutationTypes
-            .includes(mutation.type)
+        return false;
     },
 })
