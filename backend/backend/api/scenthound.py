746a168bb839b87593373562ddba3c40ccf82918
Oskari Kausiala <oskari.kausiala@karsa.fi>
2023-01-30 15:16:40 +0200

    Fix bugs in scenthound api

--- backend/backend/api/scenthound.py
+++ backend/backend/api/scenthound.py
@@ -8,21 +8,18 @@ from backend.db.conn import conn
 from backend.server import sio
 
 
+async def process_batch(sample_items):
+    for sample_item in sample_items:
+        await process_sample(sample_item)
+    return
+
 async def process_sample(sample_item):
     try:
-        filename = sample_item['filename']
-        [instrument] = pd.read_sql(f"""--sql
-                SELECT instrument
-                FROM sample_file
-                WHERE filename = ?
-                """,
-                conn,
-                params=[filename]
-            )['instrument'].tolist()
         await calibration_mz_calibrate_sample(None, sample_item, {})
-        await match_item_compute(sample_item['sample_item_id'])
-    except:
+        await match_item_compute(sample_item)
+    except Exception as e:
         print("Failed to process sample %s" %sample_item['filename'])
+        print(e)
 
 
 async def scenthound_process_sample(sid, sample_item):
@@ -37,16 +34,13 @@ async def scenthound_process_samples(sid, sample_items):
     sample_item_df = sample_item_create(sample_items)
     sample_items = sample_item_df.to_dict('records')
 
-    process_tasks = [
-        sio.start_background_task(
-            process_sample, sample_item
-        )
-        for sample_item in sample_items
-    ]
-    await asyncio.gather(*process_tasks)
+    process_task = sio.start_background_task(
+        process_batch, sample_items
+    )
+    await asyncio.gather(process_task)
     sample_batch_id = sample_item_df['sample_batch_id'].tolist()[0]
     await sio.emit(
         'sample_batch_reload',
         room=sample_batch_id,
         namespace='/'
-        )
+    )
