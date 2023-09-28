94a5e0eceab8b5fbf23b10af2b7f25e911c23f80
Oskari Kausiala <oskari.kausiala@karsa.fi>
2023-09-28 17:05:01 +0300

    Fix build and deploy scripts

--- scripts/deploy/dev.win/_start.cmd
+++ scripts/deploy/dev.win/_start.cmd
@@ -7,22 +7,12 @@ pushd frontend
 start "mascope-frontend" cmd /k yarn serve --host
 popd
 
-pushd backend
-start "mascope-backend-file-converter-kltof1" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/KLTOF1.yaml
-popd
-pushd backend
-start "mascope-backend-file-converter-kltof2" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/KLTOF2.yaml
-popd
-pushd backend
-start "mascope-backend-file-converter-korbi1" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/KORBI1.yaml
-popd
-
-pushd backend
-start "mascope-backend-file-downloader-kltof1" cmd /k poetry run file-downloader --config ./backend/service/file_downloader_config/KLTOF1.yaml
-popd
-pushd backend
-start "mascope-backend-file-downloader-kltof2" cmd /k poetry run file-downloader --config ./backend/service/file_downloader_config/KLTOF2.yaml
-popd
-pushd backend
-start "mascope-backend-file-downloader-korbi1" cmd /k poetry run file-downloader --config ./backend/service/file_downloader_config/KORBI1.yaml
-popd
+@REM pushd backend
+@REM start "mascope-backend-file-converter-kltof1" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/KLTOF1.yaml
+@REM popd
+@REM pushd backend
+@REM start "mascope-backend-file-converter-kltof2" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/KLTOF2.yaml
+@REM popd
+@REM pushd backend
+@REM start "mascope-backend-file-converter-korbi1" cmd /k poetry run file-converter --config ./backend/service/file_converter_config/KORBI1.yaml
+@REM popd
