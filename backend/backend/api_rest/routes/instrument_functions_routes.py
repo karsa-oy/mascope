8a2dfe1fd1db572198e8d49d824aeeb26ebdef46
Philip Chernonog <philip.chernonog@karsa.fi>
2023-11-10 15:01:39 +0200

    refactoring fetching instrument function by filename
    Export batch peak list blocks server. #125

--- backend/backend/api_rest/routes/instrument_functions_routes.py
+++ backend/backend/api_rest/routes/instrument_functions_routes.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from ..controllers.instrument_functions_controller import (
-    get_instrument_function_by_id,
     get_instrument_functions,
+    get_instrument_function,
 )
 
 instrument_functions_router = APIRouter()
@@ -18,6 +18,13 @@ async def get_instrument_functions_route(
     return await get_instrument_functions(instrument, sort, order, page, limit)
 
 
-@instrument_functions_router.get("/api/instrument_functions/{instrument_function_id}")
-async def get_instrument_function_by_id_route(instrument_function_id: str):
-    return await get_instrument_function_by_id(instrument_function_id)
+@instrument_functions_router.get("/api/instrument_function")
+async def get_instrument_function_route(
+    filename: str = Query(None, description="The filename to query for"),
+    instrument_function_id: str = Query(
+        None, description="The instrument function ID to query for"
+    ),
+):
+    return await get_instrument_function(
+        filename=filename, instrument_function_id=instrument_function_id
+    )
