e9eaef8ad9d55e28fc63fd43cbeaf241da0ea4e7
Philip Chernonog <philip.chernonog@karsa.fi>
2024-03-31 14:02:49 +0300

    API. Error handling for target_isotopes resource
    - Utilized api_route and api_controller decorators from api_features.py to standardize response structure and handle exceptions
    - Added documentation
    - The query parameters are now passed using the Pydantic model (e.g. GetTargetIsotopesQueryParams), which ensures that all required parameters are provided and correctly validated, in the routes leveraging FastAPI's dependency injection system “Depends()” for better maintainability and readability.
    
    Design error handling pattern. Issue #151

--- backend/backend/api_rest/routes/target_isotopes_routes.py
+++ backend/backend/api_rest/routes/target_isotopes_routes.py
@@ -1,56 +1,25 @@
-from fastapi import APIRouter, Query
-from typing import List, Optional
-
+from fastapi import APIRouter, Depends
+from ..utils.api_features import api_route
 from ..controllers.target_isotopes_controller import (
     get_target_isotope,
     get_target_isotopes,
+)
+from ..models.pydantic_models.target_isotope_pydantic_model import (
+    GetTargetIsotopesQueryParams,
 )
 
 target_isotopes_router = APIRouter()
 
 
 @target_isotopes_router.get("/api/target_isotopes")
+@api_route()
 async def get_target_isotopes_route(
-    target_ion_id: str = Query(None, description="Filter by target ion ID."),
-    min_mz: float = Query(None, description="Minimum m/z value for filtering."),
-    max_mz: float = Query(None, description="Maximum m/z value for filtering."),
-    min_relative_abundance: float = Query(
-        None, description="Minimum relative abundance for filtering."
-    ),
-    max_relative_abundance: float = Query(
-        None, description="Maximum relative abundance for filtering."
-    ),
-    target_compound_ids: List[str] = Query(
-        default=[], description="List of target compound IDs to filter isotopes."
-    ),
-    ionization_mechanism_ids: List[str] = Query(
-        default=[], description="List of ionization mechanism IDs to filter isotopes."
-    ),
-    sample_batch_id: str = Query(
-        None,
-        description="ID of the sample batch for filtering the associated to batch isotopes.",
-    ),
-    sort: str = Query(None, description="Field to sort by."),
-    order: str = Query(None, description="Order of sorting ('asc' or 'desc')."),
-    page: int = Query(0, description="Pagination page."),
-    limit: int = Query(1000000, description="Number of items per page."),
+    query_params: GetTargetIsotopesQueryParams = Depends(),
 ):
-    return await get_target_isotopes(
-        target_ion_id,
-        min_mz,
-        max_mz,
-        min_relative_abundance,
-        max_relative_abundance,
-        target_compound_ids,
-        ionization_mechanism_ids,
-        sample_batch_id,
-        sort,
-        order,
-        page,
-        limit,
-    )
+    return await get_target_isotopes(**query_params.dict())
 
 
 @target_isotopes_router.get("/api/target_isotopes/{target_isotope_id}")
+@api_route()
 async def get_target_isotope_route(target_isotope_id: str):
     return await get_target_isotope(target_isotope_id)
