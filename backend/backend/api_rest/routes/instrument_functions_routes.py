0c2ca573759fa2c5c6d3b855f49b87c071aa4385
Philip Chernonog <philip.chernonog@karsa.fi>
2024-03-31 14:11:06 +0300

    API. Error handling for instrument_functions resource
    - Utilized api_route and api_controller decorators from api_features.py to standardize response structure and handle exceptions
    - Added documentation
    - The query parameters are now passed using the Pydantic model (e.g. GetInstrumentFunctionsQueryParams, GetInstrumentFunctionQueryParams ), which ensures that all required parameters are provided and correctly validated, in the routes leveraging FastAPI's dependency injection system “Depends()” for better maintainability and readability.
    
    Design error handling pattern. Issue #151

--- backend/backend/api_rest/routes/instrument_functions_routes.py
+++ backend/backend/api_rest/routes/instrument_functions_routes.py
@@ -1,40 +1,31 @@
-from fastapi import APIRouter, Query, HTTPException
+from fastapi import APIRouter, Depends
+from ..utils.api_features import api_route
 from ..controllers.instrument_functions_controller import (
     get_instrument_functions,
     get_instrument_function,
+)
+from ..models.pydantic_models.instrument_function_pydantic_model import (
+    GetInstrumentFunctionsQueryParams,
+    GetInstrumentFunctionQueryParams,
 )
 
 instrument_functions_router = APIRouter()
 
 
 @instrument_functions_router.get("/api/instrument_functions")
+@api_route()
 async def get_instrument_functions_route(
-    instrument: str = None,
-    sort: str = None,
-    order: str = None,
-    page: int = 0,
-    limit: int = 100,
+    query_params: GetInstrumentFunctionsQueryParams = Depends(),
 ):
-    return await get_instrument_functions(instrument, sort, order, page, limit)
+    return await get_instrument_functions(**query_params.dict())
 
 
 @instrument_functions_router.get("/api/instrument_functions/")
+@api_route()
 async def get_instrument_function_route(
-    filename: str = Query(
-        None,
-        description="The filename to query for the last instrument function of the instrument",
-    ),
-    instrument_function_id: str = Query(
-        None, description="The instrument function ID to query for"
-    ),
+    query_params: GetInstrumentFunctionQueryParams = Depends(),
 ):
-    if (instrument_function_id and filename) or (
-        not instrument_function_id and not filename
-    ):
-        raise HTTPException(
-            status_code=400,
-            detail="Must provide either filename either instrument_function_id.",
-        )
     return await get_instrument_function(
-        filename=filename, instrument_function_id=instrument_function_id
+        filename=query_params.filename,
+        instrument_function_id=query_params.instrument_function_id,
     )
