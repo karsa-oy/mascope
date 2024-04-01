9e8dbcad9882dc6ef17c468ee0344ef14ac12187
Philip Chernonog <philip.chernonog@karsa.fi>
2024-04-01 16:59:43 +0300

    API. Improved error handling general logic improvement / small fixes
    - Introduced `api_controller_background_task` in `api_features.py`, encapsulating background task management with automated success and error handling, and streamlined Socket.IO event emissions.
    - Implemented `success_emit_events` and `error_emit_events` within `api_controller_background_task` to centralize event emission logic
    - Leveraged `default_payload` in background tasks for unified notification structure.
    - Introduced a utility function `beautify_func_name` to improve readability of auto-generated messages and logs by converting function names to more user-friendly strings.
    - Imroved exception handling in `exceptions.py` by adding support for `AttributeError` and `RuntimeError`.
    
    Design error handling pattern. Issue #151

--- backend/backend/api_rest/controllers/sample_files_controller.py
+++ backend/backend/api_rest/controllers/sample_files_controller.py
@@ -335,8 +335,8 @@ async def get_sample_file_peak_timeseries(
             "data": {
                 "mz": None,
                 "intensity": [],
+                "time": [],
             },
-            "time": [],
         }
 
     return {
