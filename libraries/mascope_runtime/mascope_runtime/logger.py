c56daf9765f781f1f920a2bf5d1d1d846e12faaa
Matan Bendix Shenhav <matan@shenhav.fyi>
2024-07-10 18:43:20 +0200

    Fix logging file resolution

--- libraries/mascope_runtime/mascope_runtime/logger.py
+++ libraries/mascope_runtime/mascope_runtime/logger.py
@@ -1,39 +1,22 @@
 import logging
 from rich.logging import RichHandler
-from pydantic import BaseModel
-from typing import Callable
 
 from .modules import modules
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(message)s",
-    datefmt="[%X]",
-    handlers=[
-        RichHandler(
-            markup=True,
-            rich_tracebacks=True
-        )
-    ]
-)
-
-class MascopeLogger(BaseModel):
-    info: Callable
-    error: Callable
-    warning: Callable
-    debug: Callable
-    critical: Callable
-
 def create(name: str, color: str, max_length: int):
-    format=lambda msg: f'[{color}] {name.center(max_length, ' ').upper()} [/{color}] {msg}'
-    log=logging.getLogger(name)
-    return MascopeLogger(
-        info = lambda msg: log.info(format(msg)),
-        error = lambda msg: log.error(format(msg)),
-        warning = lambda msg: log.warning(format(msg)),
-        debug = lambda msg: log.debug(format(msg)),
-        critical = lambda msg: log.critical(format(msg))
+    prefix=f'[{color}] {name.center(max_length, ' ').upper()} [/{color}]'
+    logging.basicConfig(
+        level=logging.INFO,
+        format=prefix + "%(message)s",
+        datefmt="[%X]",
+        handlers=[
+            RichHandler(
+                markup=True,
+                rich_tracebacks=True
+            )
+        ]
     )
+    return logging.getLogger(name)
 
 def service(name: str):
     max_length=max(*map(lambda p: len(p['name']), modules))
