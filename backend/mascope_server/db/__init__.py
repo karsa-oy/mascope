916b7250e5f9ff1ac1722a31eb4191934fc3bdf7
Philip Chernonog <philip.chernonog@karsa.fi>
2024-07-11 10:32:18 +0300

    Fix the database migration bugs, clean up db init file

--- backend/mascope_server/db/__init__.py
+++ backend/mascope_server/db/__init__.py
@@ -1,6 +1,5 @@
 import os
 import re
-import traceback
 import shutil
 import inspect
 from datetime import datetime
@@ -11,10 +10,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import text
 
 from mascope_server.config import config
-
 import mascope_runtime as runtime
-logger = runtime.logger.service('backend')
 
+logger = runtime.logger.service("backend")
+
+# Initialize global variables at module load
+ASYNC_SESSION = None  # Global variable for session
+db_dir = config.server.database
+
+
+# Database utility functions
 def get_available_db_version():
     migrations_dir = os.path.join(os.path.dirname(__file__), "migration")
     files = os.listdir(migrations_dir)
@@ -36,48 +41,6 @@ def get_current_db_version():
     return v
 
 
-def migrate(current_version, target_version):
-    logger.info("Executing migration pathway")
-    if current_version == 0 and not os.path.exists(config.server.database):
-        os.mkdir(config.server.database)
-    while current_version < target_version:
-        next_version = current_version + 1
-        try:
-            migration = import_module(f"mascope_server.db.migration.v{next_version}")
-        except Exception as error:
-            logger.error(error)
-        migration_label = f"from v{current_version} to v{next_version}"
-        logger.info(f"Attempting to migrate mascope database {migration_label}")
-        try:
-            migration.run()
-        except Exception as error:
-            logger.error(f"Migration {migration_label} failed!")
-            failed_db_path = os.path.join(config.server.database, f"mascope.v{next_version}.db")
-            debug_db_path = os.path.join(config.server.database, "mascope.debug.db")
-            if os.path.exists(failed_db_path):
-                os.rename(failed_db_path, debug_db_path)
-            logger.error(error)
-            logger.error(f"A copy failed target database is found at {debug_db_path}")
-            raise RuntimeError("Database migration failed")
-            break
-        else:
-            logger.info(f"Migration {migration_label} succeded!")
-            current_version = get_current_db_version()
-    if current_version == target_version:
-        logger.info("Migration pathway succesful: database is now up-to-date.")
-    return current_version
-
-
-async def test_database_connection():
-    try:
-        # create a new session and close it
-        async with async_session() as session:
-            await session.execute(text("SELECT 1"))
-        logger.info("Database connection established successfully.")
-    except Exception as e:
-        logger.error("Error while establishing the database connection: ", e)
-
-
 def create_db_backup(db_path, operation):
     """Creates a timestamped backup of the database."""
     data_path = os.path.dirname(db_path)
@@ -95,6 +58,7 @@ def create_db_backup(db_path, operation):
     return backup_db_path
 
 
+# Migration functions
 async def run_migration_script(migration):
     """
     Executes a migration script, handling both synchronous and asynchronous run functions.
@@ -141,6 +105,7 @@ async def migrate(current_version, target_version):
     return current_version
 
 
+# Database configuration and session management
 def configure_database_engine(version):
     db_path = os.path.join(db_dir, f"mascope.v{version}.db")
 
@@ -159,16 +124,11 @@ def configure_database_engine(version):
     ASYNC_SESSION = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
-# Initialize global variables at module load
-ASYNC_SESSION = None  # Global variable for session
-
-db_dir = config.server.database
-
-
 def async_session():
     return ASYNC_SESSION()
 
 
+# Initialization and main interface functions
 async def init_db():
     try:
         logger.info("Initializing mascope database")
@@ -186,19 +146,11 @@ async def init_db():
     await test_database_connection()
 
 
-# env vars
-current_version = get_current_db_version()
-db_path = os.path.join(config.server.database, f"mascope.v{current_version}.db")
-
-# Database configuration
-DATABASE_URL = f"sqlite+aiosqlite:///{db_path}"
-engine = create_async_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,  # Check connection liveness before using a connection from the pool
-    # echo=True, # TODO_debug_mode Enable logging of all SQL queries for debugging purposes
-    connect_args={
-        "timeout": 15
-    },  # Set a timeout of 15 seconds for establishing connections and waiting for table locks
-)
-
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+async def test_database_connection():
+    try:
+        # create a new session and close it
+        async with async_session() as session:
+            await session.execute(text("SELECT 1"))
+        logger.info("Database connection established successfully.")
+    except Exception as e:
+        logger.error("Error while establishing the database connection: ", e)
