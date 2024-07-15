bfaca84ac04195bca162e1de81cbdaafddfb4310
Matan Bendix Shenhav <matan@shenhav.fyi>
2024-07-15 11:28:58 +0200

    Introduce threaded dev server option

--- tooling/cli/mascope_cli/cmd/dev.py
+++ tooling/cli/mascope_cli/cmd/dev.py
@@ -1,8 +1,6 @@
 import typer, os
 from typing import List, Dict, Annotated
 
-import time
-
 import mascope_runtime as runtime
 
 from .. import lib
@@ -20,16 +18,66 @@ def main():
     🏗️ Manage your development environment
     """
 
-@dev.command()
-def run(processes: Annotated[List[str], typer.Argument()]=None, kill_others: bool=False, all: bool=False):
-    """
-    Run your development environment
+def run_threaded(processes: Annotated[List[str], typer.Argument()]=None, all: bool=False):
+    import shlex
+    import subprocess
+    import threading
 
-    Runs the backend and frontend by default. Use the --all flag to run all
-    services, or pass service names as arguments.
+    keep_running = True
 
-    To list available services, run 'mascope modules --runnable'.
-    """
+    def run_thread(module):
+        args = shlex.split(module['run'])
+        proc = subprocess.Popen(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=os.path.join(mascope_path, *module['path'])
+        )
+        assert proc.stdout
+        try:
+            while keep_running:
+                line = proc.stdout.readline().decode("utf-8", errors="replace")
+                if line != "":
+                    print(line)
+        except Exception:
+            pass
+        proc.terminate()  # ensure its dead
+        print(f"{module['name']} exited")
+
+    runnable=[mod['name'] for mod in runtime.modules if mod['run']]
+    mod_names=(processes                 # use argument if provided
+        or (runnable if all else None)   # if --all option used, run anything runnable
+        or ['backend', 'frontend']       # otherwise use common defaults
+    )
+    # select processes
+    selected=[mod
+        for mod in runtime.modules
+        if mod['name'] in mod_names
+    ]
+    # set config env var
+    config=runtime.config.autoload()
+    os.environ['MASCOPE_CONFIG'] = config.model_dump_json()
+
+    # run commands in threads
+    threads = [
+        threading.Thread(
+            target=run_thread,
+            args=[mod]
+        ) for mod in selected
+    ]
+    for thread in threads:
+        thread.start()
+    # run loop
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        keep_running = False
+        # wait for proper exit
+        for thread in threads:
+            thread.join()
+
+def run_concurrently(processes: Annotated[List[str], typer.Argument()]=None, kill_others: bool=False, all: bool=False):
     runnable=[mod['name'] for mod in runtime.modules if mod['run']]
     mod_names=(processes                 # use argument if provided
         or (runnable if all else None)   # if --all option used, run anything runnable
@@ -50,6 +98,22 @@ def run(processes: Annotated[List[str], typer.Argument()]=None, kill_others: boo
     options='--kill-others' if kill_others else ''
     # run command
     lib.run(f'concurrently.cmd --raw {options} {names} {colors} {cmds}')
+
+
+@dev.command()
+def run(processes: Annotated[List[str], typer.Argument()]=None, kill_others: bool=False, all: bool=False, threaded: bool=False):
+    """
+    Run your development environment
+
+    Runs the backend and frontend by default. Use the --all flag to run all
+    services, or pass service names as arguments.
+
+    To list available services, run 'mascope modules --runnable'.
+    """
+    if (threaded):
+        run_threaded(processes, all)
+    else:
+        run_concurrently(processes, kill_others, all)
 
 # INSTALL
 
@@ -88,3 +152,4 @@ def install(lock: bool = False):
     """
     for mod in runtime.modules:
         install_mod(mod, lock)
+
