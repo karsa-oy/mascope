16359e75c6a7a71e536c9b6db26c61ab37340e26
Matan Bendix Shenhav <matan@shenhav.fyi>
2024-07-17 11:46:44 +0200

    Add loglevel option to runtime.logger.service

--- libraries/mascope_runtime/mascope_runtime/logger.py
+++ libraries/mascope_runtime/mascope_runtime/logger.py
@@ -4,32 +4,32 @@ from rich.console import Console
 from rich.theme import Theme
 
 console = Console(
-    theme=Theme({
-        "logging.level.info": "deep_sky_blue1",
-        "logging.level.warning": "orange1",
-        "logging.level.error": "bold red"
-    })
+    theme=Theme(
+        {
+            "logging.level.info": "deep_sky_blue1",
+            "logging.level.warning": "orange1",
+            "logging.level.error": "bold red",
+        }
+    )
 )
 
 from .modules import modules
 
-def create(name: str, color: str, max_length: int, markup: bool = True):
-    prefix=f'[{color}] {name.center(max_length, ' ')} [/{color}]'
+
+def create(
+    name: str, color: str, max_length: int, level=logging.INFO, markup: bool = True
+):
+    prefix = f"[{color}] {name.center(max_length, " ")} [/{color}]"
     logging.basicConfig(
-        level=logging.INFO,
+        level=level,
         format=prefix + "%(message)s",
         datefmt="[%X]",
-        handlers=[
-            RichHandler(
-                markup=markup,
-                rich_tracebacks=True,
-                console=console
-            )
-        ]
+        handlers=[RichHandler(markup=markup, rich_tracebacks=True, console=console)],
     )
     return logging.getLogger(name)
 
-def service(name: str, markup: bool = True):
-    max_length=max(*map(lambda p: len(p['name']), modules))
-    mod=next(m for m in modules if m['name'] == name)
-    return create(mod['name'], mod['color'], max_length, markup)
+
+def service(name: str, level=logging.INFO, markup: bool = True):
+    max_length = max(*map(lambda p: len(p["name"]), modules))
+    mod = next(m for m in modules if m["name"] == name)
+    return create(mod["name"], mod["color"], max_length, level, markup)
