b9332fede8e7437f0a782422529186a975e14e17
Philip Chernonog <philip.chernonog@karsa.fi>
2024-07-17 16:02:49 +0300

    Enable network-wide access for Vite server
    Update Vite configuration to listen on all network interfaces enabling accessing of the vite server not just locally, but also over a local area network. Needed for develop server.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { ViteToml } from 'vite-plugin-toml'
@@ -8,13 +7,19 @@ export default defineConfig({
   plugins: [ViteToml(), vue()],
   resolve: {
     alias: {
+      // This alias configuration helps in resolving paths relative to the src directory.
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    // Note: Configures the server to listen on all network interfaces. This setting is needed for accessing
+    // the development server (http://192.168.1.249:5173/) not just locally, but also over a local area network.
+    host: '0.0.0.0'
   },
   build: {
     chunkSizeWarningLimit: 600,
     cssCodeSplit: false,
     target: 'esnext'
   },
-  envPrefix: 'MASCOPE_'
+  envPrefix: 'MASCOPE_' // Prefix for environment variables used in the project.
 })
