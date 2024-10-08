23abc32e88f9fd5b82a52e6165d47561647c9afe
oskarsari <79190195+oskarsari@users.noreply.github.com>
2024-10-08 16:37:34 +0300

    Fix sample batch update (#555)
    Fixes sample batch update in case where the batch doesn't have `calibration_ion_mechanism` store in `build_params `in the db

--- backend/mascope_server/api/controllers/sample/batches/sample_batches_controller.py
+++ backend/mascope_server/api/controllers/sample/batches/sample_batches_controller.py
@@ -409,7 +409,7 @@ async def update_sample_batch(
         # ensure batch reload is triggered if calibration
         # mechanisms are changed
         existing_calibration_mechanisms = set(
-            existing_sample_batch.build_params["calibration_ion_mechanisms"]
+            existing_sample_batch.build_params.get("calibration_ion_mechanisms", [])
         )
         new_calibration_mechanisms = set(
             sample_batch_update_body.build_params.calibration_ion_mechanisms
