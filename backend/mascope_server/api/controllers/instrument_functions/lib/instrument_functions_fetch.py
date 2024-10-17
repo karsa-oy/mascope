05b0f1b167f5c5533278eb4ed3d5815a1a49b447
Oskari Kausiala <oskari.kausiala@karsa.fi>
2024-10-17 15:21:00 +0300

    Comment resolution function selection

--- backend/mascope_server/api/controllers/instrument_functions/lib/instrument_functions_fetch.py
+++ backend/mascope_server/api/controllers/instrument_functions/lib/instrument_functions_fetch.py
@@ -24,11 +24,14 @@ async def read_instrument_functions(filename):
     peakshape = instrument_functions["peakshape"]
     R_p = instrument_functions["resolution_function"]
     if len(R_p) == 1:
+        # Use native Orbitrap resolution function
         p1 = R_p[0]
         R = lambda m: r_orbi(m, p1)
     elif len(R_p) == 2:
+        # Use resolution function from Junninen's thesis for TOF
         p1, p2 = R_p
         R = lambda m: m / (p1 * m + p2)
     elif len(R_p) == 3:
+        # Use 2nd order polynomial (backwards compatibility for Orbitrap) TODO: legacy
         R = np.poly1d(R_p)
     return peakshape, R
