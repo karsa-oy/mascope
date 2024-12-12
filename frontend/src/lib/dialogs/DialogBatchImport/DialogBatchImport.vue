bc1342e891f058326ca4fb992a99ecd27cec0efa
Matan Bendix Shenhav <matan@shenhav.fyi>
2024-12-12 16:49:15 +0100

    Batch import dialog: validation composable
    This seperates the batch import validation logic into a seperate
    file using a composable. VueUse is installed to facilitate seamless
    reactivity.

--- frontend/src/lib/dialogs/DialogBatchImport/DialogBatchImport.vue
+++ frontend/src/lib/dialogs/DialogBatchImport/DialogBatchImport.vue
@@ -20,14 +20,10 @@ import { ref, reactive, computed, watch } from 'vue'
 import { BaseClipboardContext } from '@/lib/base'
 import { fromSpreadsheet, parseAutosamplerCsv, parseGenericCsv } from '@/lib/table'
 import { genId } from '@/lib/utils'
-import {
-  sampleTypes,
-  sampleTypesFilterIdRequired,
-  sampleTypesFilterIdOptional,
-  sampleTypesFilterIdNotAllowed
-} from '@/lib/constants'
 
 import { useApp } from '@/stores'
+
+import { useValidation } from './validation.js'
 
 const confirm = useConfirm()
 
@@ -63,16 +59,8 @@ const filters = computed(() => {
     : generated
 })
 
-const validation = reactive({
-  rows: {
-    issues: [],
-    passed: false
-  },
-  cols: {
-    issues: [],
-    passed: false
-  }
-})
+const validation = useValidation({ imported })
+
 const title = computed(() => {
   const name = app.data.batch.focused?.sample_batch_name ?? 'selected'
   return imported.type == 'autosampler'
@@ -119,8 +107,7 @@ function init(active) {
   imported.items = []
   imported.filterId = ''
   imported.type = null
-  validation.cols.passed = false
-  validation.rows.passed = false
+  validation.reset()
 }
 
 // processing
@@ -133,7 +120,7 @@ async function parse(text) {
   imported.type = cols.some((col) => autosamplerKeys.includes(col.field))
     ? 'autosampler'
     : 'general'
-  validateColumns(cols)
+  validation.cols.execute({ cols })
   if (!validation.cols.passed) return
   // rows
   if (imported.type === 'autosampler') {
@@ -183,161 +170,24 @@ function preprocess() {
       sample_batch_id: app.data.batch.focused.sample_batch_id
     }))
   }
-  validateRows()
-}
-
-// validation
-function validateColumns(cols) {
-  validation.cols.issues = []
-  // skip column validation for autosampler report import
-  if (imported.type === 'autosampler') {
-    validation.cols.passed = true
-    return
-  }
-  // Ensure all columns are labeled
-  const unlabeledCols = cols.filter((col) => !col.label.trim())
-  if (unlabeledCols.length > 0) {
-    validation.cols.issues.push('All columns should be labeled.')
-  }
-  // Check for duplicated column names
-  const columnNameCounts = {}
-  cols.forEach((col) => {
-    const label = col.label.trim()
-    columnNameCounts[label] = (columnNameCounts[label] || 0) + 1
-  })
-  for (const label in columnNameCounts) {
-    if (columnNameCounts[label] > 1) {
-      validation.cols.issues.push(
-        `Column label "${label}" is duplicated, each column should have the unique label`
-      )
-    }
-  }
-  // Check for empty column labels and specific naming conventions
-  if (!cols[0] || cols[0].label.trim() === '') {
-    validation.cols.issues.push(
-      "The first column should be labeled to indicate it contains sample names (e.g., 'Sample Name', 'Name')."
-    )
-  }
-  if (cols.length > 1 && (!cols[1] || cols[1].label.trim() === '')) {
-    validation.cols.issues.push(
-      "The second column should be labeled to indicate it contains sample types (e.g., 'Sample Type', 'Type')."
-    )
-  }
-  if (cols.length > 2 && (!cols[2] || cols[2].label.trim() === '')) {
-    validation.cols.issues.push(
-      "The third column should be labeled to indicate it contains filter IDs (e.g., 'Filter ID', 'Filter')."
-    )
-  }
-  // Identify unlabeled sample item attribute columns and list their indices
-  const unlabeledAttributeColsIndices = cols
-    .slice(3)
-    .map((col, index) => (!col.label.trim() ? index + 4 : null))
-    .filter((index) => index !== null)
-
-  if (unlabeledAttributeColsIndices.length === 1) {
-    const columnIndex = unlabeledAttributeColsIndices[0]
-    validation.cols.issues.push(
-      `Column ${columnIndex} is unlabeled. Each column after the third must be labeled to indicate the name of the sample item attribute it contains.`
-    )
-  } else if (unlabeledAttributeColsIndices.length > 1) {
-    const formattedIndices = unlabeledAttributeColsIndices.join(', ')
-    validation.cols.issues.push(
-      `Columns ${formattedIndices} should be labeled to indicate the name of the sample item attribute it contains.`
-    )
-  }
-
-  // Show warning notification if there are any failed validations
-  if (validation.cols.issues.length > 0) {
-    tab.value = 'issues'
-    validation.cols.passed = false
-  } else {
-    validation.cols.passed = true
-  }
-}
-
-const friendlyType = (sample) => `'${sample.sample_item_type.replaceAll('_', ' ').toLowerCase()}'`
-const friendlyTypes = sampleTypes
-  .map((type) => `'${type.replaceAll('_', ' ').toLowerCase()}'`)
-  .join(', ')
-
-function validateRows() {
-  validation.rows.issues = []
-  let key = 0
-  if (!imported.items.length > 0) return
-  // Check for mismatch in the number of samples and files
-  if (imported.items.length !== props.files.length) {
-    validation.rows.issues.push({
-      key,
-      sample: null,
-      failures: [
-        `The number of pasted samples (${imported.items.length}) doesn't line up with the total number of selected files (${props.files.length}).
-          Please ensure each pasted sample corresponds to a selected file.`
-      ]
-    })
-  }
-  // Iterate over each sample item to validate data
-  for (const item of imported.items) {
-    key++
-    let failures = []
-    // Validate sample type
-    if (!sampleTypes.includes(item.sample_item_type)) {
-      failures.push(
-        `Sample type '${friendlyType(item)}' isn't recognized,` +
-          `please use one of the accepted types: ${friendlyTypes}.`
-      )
-      // Add recommendation info if not already present
-      const allowedTypes =
-        `Please use one of the allowed sample types: ${friendlyTypes}.` +
-        ` You can leave this field empty, sample type will be set to 'unknown'. `
-      if (!validation.rows.issues.includes(allowedTypes)) {
-        validation.rows.issues.push(allowedTypes)
-      }
-    }
-    // Validate filter ID presence based on sample type
-    if (sampleTypesFilterIdNotAllowed.includes(item.sample_item_type) && item.filter_id) {
-      failures.push(`Filter ID should not be provided for sample type '${friendlyType(item)}'.`)
-    } else if (sampleTypesFilterIdRequired.includes(item.sample_item_type) && !item.filter_id) {
-      failures.push(`Filter ID must be provided for sample type '${friendlyType(item)}'.`)
-    }
-    // Validate filter ID format if present
-    if (item.filter_id && !/^[0-9A-Z]{6}$/.test(item.filter_id)) {
-      failures.push(
-        `The filter ID '${item.filter_id}' is incorrectly formatted: ` +
-          `filter ID must be consist of exactly 6 letters and/or numbers,` +
-          ` e.g XYZ123, 420fyi, 1a2B3c, 123456, QWERTY.`
-      )
-
-      // Add recommendation info if not already present
-      const allowedFilterIdInfo = `Filter ID: ensure it is exactly 6 characters long and only contains uppercase letters and numbers.
-          You can leave this field empty, filter ID will be generated automatically.`
-      if (!validation.rows.issues.includes(allowedFilterIdInfo)) {
-        validation.rows.issues.push(allowedFilterIdInfo)
-      }
-    }
-    // If there are failures, add them to the failedValidations array with the sample name
-    if (failures.length > 0) {
-      validation.rows.issues.push({
-        key,
-        sample: item.sample_item_name,
-        failures
-      })
-    }
-  }
-  // Show warning notification if there are any failed validations
-  if (validation.rows.issues.length > 0) {
-    tab.value = 'issues'
-    validation.rows.passed = false
-  } else {
-    validation.rows.passed = true
-  }
+  validation.rows.execute({ files: props.files })
 }
 
 watch(
-  () => validation.rows.passed && validation.cols.passed,
+  () => validation.passed,
   (passed) => {
     if (tab.value == 'issues' && passed) {
       // switch to data data
       tab.value = 'data'
+    }
+  }
+)
+
+watch(
+  () => validation.issues,
+  (issues) => {
+    if (issues > 0 && tab.value == 'data') {
+      tab.value = 'issues'
     }
   }
 )
@@ -416,7 +266,7 @@ watch(
         <TabPanel value="issues">
           <Panel>
             <ScrollPanel style="height: 300px; max-width: 80vw">
-              <template v-if="validation.cols.issues.length > 0">
+              <section v-if="validation.cols.issues.length > 0">
                 <b>Column issues:</b>
                 <Message
                   icon="pi pi-exclamation-circle"
@@ -427,8 +277,8 @@ watch(
                 >
                   {{ msg }}
                 </Message>
-              </template>
-              <template v-if="validation.rows.issues.length > 0">
+              </section>
+              <section v-if="validation.rows.issues.length > 0">
                 <b>Row issues</b>
                 <template v-for="issue in validation.rows.issues" :key="issue.key">
                   <p v-if="issue.sample">
@@ -444,7 +294,7 @@ watch(
                     {{ failure }}
                   </Message>
                 </template>
-              </template>
+              </section>
             </ScrollPanel>
           </Panel>
         </TabPanel>
@@ -467,7 +317,7 @@ watch(
         <Button label="Cancel" severity="secondary" @click="visible = false" />
         <Button
           :label="`Process (${imported.items.length})`"
-          :disabled="!validation.rows.passed"
+          :disabled="!validation.passed"
           @click="
             () => {
               confirm.require({
