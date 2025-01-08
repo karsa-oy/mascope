1ee8bc256d5da2bb7e35999bc8bd5e34c144e0b8
Oskari Kausiala <oskari.kausiala@karsa.fi>
2025-01-08 15:25:47 +0200

    Update generate_cert script
    - Install OpenSSL if not yet installed
    - Extend certificate validity to 365 days

--- setup/generate_cert.ps1
+++ setup/generate_cert.ps1
@@ -1,14 +1,25 @@
-# generate self-signed certificate
+# Script to generate a self-signed SSL certificate for testing Mascope "prod" mode
+# in local development environment
+
+# Check if certificate already exists
 if (Test-Path "${env:MASCOPE_PATH}/secrets/mascope.app.*") {
     Write-Host "Found an existing certificate. Exiting..."
     return
- }
+}
+
+# Install OpenSSL
+if ( !( Get-Command openssl -ErrorAction SilentlyContinue) ) {
+    Write-Host "Installing OpenSSL..."
+    winget install --id ShiningLight.OpenSSL
+}
+
+# Generate the key
 openssl req `
     -x509 `
     -nodes `
     -subj "/CN=mascope.app" `
     -addext "subjectAltName=DNS:mascope.app" `
-    -days 90 `
+    -days 365 `
     -newkey rsa:2048 `
     -keyout "${env:MASCOPE_PATH}/secrets/mascope.app.key" `
     -out "${env:MASCOPE_PATH}/secrets/mascope.app.pem"
