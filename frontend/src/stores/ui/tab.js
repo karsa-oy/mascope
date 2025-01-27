5f97c3b26b98298a59c1b8cecbeb943e4e8fcb79
Matan Bendix Shenhav <matan@shenhav.fyi>
2025-01-27 16:05:17 +0100

    Make the acquisition tab default & move tab logic
    Since no batch is focused when the app is first loaded, the batch tab
    is empty by default. In order to provide a more sane default, this
    sets the acquisitions tab to be the default.
    
    Additionally the tab auto-switching logic is moved the Dashboard
    component to the tab store, and so minor bugs are resolved.

--- frontend/src/stores/ui/tab.js
+++ frontend/src/stores/ui/tab.js
@@ -1,8 +1,49 @@
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { defineStore } from 'pinia'
 
+import { useData } from '../data'
+
 export const useTab = defineStore('app.ui.tab', () => {
-  const active = ref('batch')
+  const active = ref('acquisitions')
+
+  const data = useData()
+
+  watch(
+    () => data.acquisition.mode,
+    (measuring) => {
+      if (measuring) {
+        active.value = 'acquisitions'
+      }
+    }
+  )
+  watch(
+    () => data.match.visualized.ion,
+    (visualized) => {
+      if (visualized && active.value !== 'spectrum') {
+        active.value = 'match'
+      } else {
+        if (active.value == 'match') {
+          active.value = data.batch.focused ? 'batch' : 'acquisitions'
+        }
+      }
+    }
+  )
+  watch(
+    () => data.sample.focused,
+    (sample) => {
+      if (!sample && active.value == 'spectrum') {
+        active.value = 'batch'
+      }
+    }
+  )
+  watch(
+    () => data.batch.focused,
+    (focused) => {
+      if (!focused && active.value == 'batch') {
+        active.value = 'acquisitions'
+      }
+    }
+  )
 
   return { active }
 })
