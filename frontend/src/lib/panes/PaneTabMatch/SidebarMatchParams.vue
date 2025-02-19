a78cf1b78e3f32584825ed39d4186fe25b4c9842
Matan Bendix Shenhav <matan@shenhav.fyi>
2025-02-19 12:47:16 +0100

    Fix match sidebar param reactivity

--- frontend/src/lib/panes/PaneTabMatch/SidebarMatchParams.vue
+++ frontend/src/lib/panes/PaneTabMatch/SidebarMatchParams.vue
@@ -22,70 +22,80 @@ watchEffect(() => {
   open.value = drawer.value
 })
 
-async function saveParams() {
-  confirm.require({
-    icon: 'pi pi-info-circle',
-    header: 'Saving match parameters',
-    message: `Are you sure you want to save current ${app.data.match.visualized.ion.target_ion_formula} match parameters for ${app.data.match.visualized.ion.instrument} instrument?`,
-    accept: async () => {
-      isSaving.value = true
-      await app.data.match.params.save()
-      isSaving.value = false
-    },
-    acceptProps: {
-      icon: 'pi pi-save',
-      label: 'Save'
-    },
-    rejectProps: {
-      icon: 'pi pi-times',
-      label: 'Cancel',
-      severity: 'secondary'
-    }
-  })
-}
-
-function deleteParams() {
-  confirm.require({
-    icon: 'pi pi-exclamation-triangle',
-    header: 'Deleting match parameters',
-    message: `Are you sure you want to delete ${app.data.match.visualized.ion?.target_ion_formula} match parameters for ${app.data.match.visualized.ion?.instrument} instrument?`,
-    accept: app.data.match.params.remove,
-    acceptProps: {
-      icon: 'pi pi-trash',
-      label: 'Delete',
-      severity: 'danger'
-    },
-    rejectProps: {
-      icon: 'pi pi-times',
-      label: 'Cancel',
-      severity: 'secondary'
-    }
-  })
+const key = ref(0)
+const refresh = () => {
+  key.value = Math.random()
 }
 
 const items = computed(() => [
   {
     label: 'Save parameters',
     icon: 'pi pi-save',
-    command: saveParams,
+    command: () => {
+      confirm.require({
+        icon: 'pi pi-info-circle',
+        header: 'Saving match parameters',
+        message: `Are you sure you want to save current ${app.data.match.visualized.ion.target_ion_formula} match parameters for ${app.data.match.visualized.ion.instrument} instrument?`,
+        accept: async () => {
+          isSaving.value = true
+          await app.data.match.params.save()
+          isSaving.value = false
+        },
+        acceptProps: {
+          icon: 'pi pi-save',
+          label: 'Save'
+        },
+        rejectProps: {
+          icon: 'pi pi-times',
+          label: 'Cancel',
+          severity: 'secondary'
+        }
+      })
+    },
     disabled: !app.data.match.params.changed
   },
   {
     label: 'Revert changes',
     icon: 'pi pi-undo',
-    command: app.data.match.params.revert,
+    command: () => {
+      app.data.match.params.revert()
+      refresh()
+    },
     disabled: !app.data.match.params.changed
   },
   {
     label: 'Set defaults',
     icon: 'pi pi-file-import',
-    command: app.data.match.params.reset,
+    command: () => {
+      app.data.match.params.reset()
+      refresh()
+    },
     disabled: app.data.match.params.default
   },
   {
     label: 'Delete filtering parameters',
     icon: 'pi pi-trash',
-    command: deleteParams,
+    command: () => {
+      confirm.require({
+        icon: 'pi pi-exclamation-triangle',
+        header: 'Deleting match parameters',
+        message: `Are you sure you want to delete ${app.data.match.visualized.ion?.target_ion_formula} match parameters for ${app.data.match.visualized.ion?.instrument} instrument?`,
+        accept: () => {
+          app.data.match.params.remove()
+          refresh()
+        },
+        acceptProps: {
+          icon: 'pi pi-trash',
+          label: 'Delete',
+          severity: 'danger'
+        },
+        rejectProps: {
+          icon: 'pi pi-times',
+          label: 'Cancel',
+          severity: 'secondary'
+        }
+      })
+    },
     disabled:
       app.data.match.visualized.ion?.filter_params &&
       app.data.match.visualized.ion?.instrument in app.data.match.visualized.ion.filter_params
@@ -152,6 +162,7 @@ const matchRangeMiddle = computed(
         @change="app.data.match.visualized.reload"
         :range="{ min: 0, max: 100, step: 1 }"
         small
+        :key="key"
       />
       <BaseParamField
         label="Min. isotope abundance"
@@ -161,6 +172,7 @@ const matchRangeMiddle = computed(
         disabled
         col
         small
+        :key="key"
       />
       <BaseParamField
         label="Isotope ratio tolerance"
@@ -168,6 +180,7 @@ const matchRangeMiddle = computed(
         @change="app.data.match.visualized.reload"
         :range="{ min: 0, max: 1, step: 0.05 }"
         small
+        :key="key"
       />
       <BaseParamField
         label="Min. isotope correlation"
@@ -175,6 +188,7 @@ const matchRangeMiddle = computed(
         @change="app.data.match.visualized.reload"
         :range="{ min: 0, max: 1, step: 0.1 }"
         small
+        :key="key"
       />
     </section>
     <section>
@@ -185,6 +199,7 @@ const matchRangeMiddle = computed(
         @change="app.data.match.visualized.reload"
         :range="{ min: 0, max: 10000, step: 500 }"
         small
+        :key="key"
       />
       <div class="col" style="gap: 0">
         <div class="row" :key="matchRangeMiddle">
@@ -199,6 +214,7 @@ const matchRangeMiddle = computed(
             }"
             hideSlider
             small
+            :key="key"
           />
           <BaseParamField
             label="Probable match [%]"
@@ -211,6 +227,7 @@ const matchRangeMiddle = computed(
             }"
             hideSlider
             small
+            :key="key"
           />
         </div>
         <div style="width: 100%; margin-top: 1rem" class="match-slider">
@@ -221,6 +238,7 @@ const matchRangeMiddle = computed(
             :max="1"
             :step="0.05"
             :style="`background: linear-gradient(90deg, var(--p-button-success-background) ${matchRangeMiddle}%, var(--p-button-danger-background) ${matchRangeMiddle}%)`"
+            :key="key"
           />
         </div>
       </div>
