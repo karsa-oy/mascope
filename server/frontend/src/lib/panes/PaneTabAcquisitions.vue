21c1613fd70f1953f7750708b0c0738ad1c8a712
Matan Bendix Shenhav <matan@shenhav.fyi>
2025-04-16 17:22:32 +0200

    Acquisition tab row selection and context menu
    This adds changes to the UX of the acquisition tab to conform to
    other data browsers:
    - row selection rather than checkboxes
    - context menu
    - virtual scrolling rather than pagination
    
    Download and delete actions are added to the context menu.

--- server/frontend/src/lib/panes/PaneTabAcquisitions.vue
+++ server/frontend/src/lib/panes/PaneTabAcquisitions.vue
@@ -11,10 +11,12 @@ import InputIcon from 'primevue/inputicon'
 import InputText from 'primevue/inputtext'
 import FileUpload from 'primevue/fileupload'
 import FloatLabel from 'primevue/floatlabel'
+import ContextMenu from 'primevue/contextmenu'
 
 import { runtime } from '@/lib/runtime'
 import { DialogSampleOp, DialogBatchImport, DialogFileUpload } from '@/lib/dialogs'
 
+import { api } from '@/api'
 import { useApp } from '@/stores'
 import { Message } from 'primevue'
 
@@ -34,18 +36,60 @@ const dialog = reactive({
   batchImport: false
 })
 
+const contextMenuRef = ref(null)
+const contextMenuItems = ref([
+  {
+    label: 'Download',
+    icon: 'pi pi-download',
+    command: () => {
+      api.http.post(
+        `/file/download`,
+        {
+          sample_file_ids: app.data.acquisition.selected.map(({ sample_file_id }) => sample_file_id)
+        },
+        {
+          use: 'process',
+          type: 'download_sample_files'
+        }
+      )
+    }
+  },
+  {
+    label: 'Delete',
+    icon: 'pi pi-trash',
+    command: () => {
+      const sample_file_ids = app.data.acquisition.selected.map(
+        ({ sample_file_id }) => sample_file_id
+      )
+      if (sample_file_ids.length > 0) {
+        api.http.post(
+          `/sample/files/delete`,
+          { sample_file_ids },
+          {
+            use: 'delete',
+            type: 'delete_sample_files'
+          }
+        )
+      }
+    }
+  }
+])
+const contextMenuRow = ref(null)
+
 const search = ref('')
 const polarityDropdown = ref('')
 
-const acquisitions = computed(() => 
-  app.data.acquisition.list?.filter(({ filename, polarity }) => 
-    filename.toLowerCase().includes(search.value.toLowerCase()) &&
-    (polarityDropdown.value === '' ||
-      polarityDropdown.value === '+-' ||
-      polarity === polarityDropdown.value ||
-      polarity === '+-')
-  ) ?? []
-);
+const acquisitions = computed(
+  () =>
+    app.data.acquisition.list?.filter(
+      ({ filename, polarity }) =>
+        filename.toLowerCase().includes(search.value.toLowerCase()) &&
+        (polarityDropdown.value === '' ||
+          polarityDropdown.value === '+-' ||
+          polarity === polarityDropdown.value ||
+          polarity === '+-')
+    ) ?? []
+)
 
 const uploadedFiles = ref([])
 
@@ -58,9 +102,9 @@ const hasBothPolarities = computed(() => {
 
 // We consider "+-" as a mixed polarity type
 const onlyMixedPolaritySelected = computed(() => {
-  const allMixed = app.data.acquisition.selected.every(({polarity}) => polarity === '+-')
+  const allMixed = app.data.acquisition.selected.every(({ polarity }) => polarity === '+-')
   const moreThanOneSelected = app.data.acquisition.selected.length > 1
-  const polarityNotSpecified = ['','+-'].includes(polarityDropdown.value)
+  const polarityNotSpecified = ['', '+-'].includes(polarityDropdown.value)
   return allMixed && moreThanOneSelected && polarityNotSpecified
 })
 
@@ -162,9 +206,11 @@ watchEffect(() => {
             hasBothPolarities ||
             onlyMixedPolaritySelected
           "
-          :tooltip="!app.data.batch.focused || app.data.acquisition.selected.length === 0 
-            ? 'Select acquisitions and a batch in order to process sample files' 
-            : ''"
+          :tooltip="
+            !app.data.batch.focused || app.data.acquisition.selected.length === 0
+              ? 'Select acquisitions and a batch in order to process sample files'
+              : ''
+          "
           @click="
             () => {
               if (app.data.acquisition.focused) {
@@ -181,40 +227,64 @@ watchEffect(() => {
       <DataTable
         v-if="acquisitions?.length"
         v-model:selection="app.data.acquisition.selected"
+        v-model:contextMenuSelection="contextMenuRow"
         :value="acquisitions"
         :totalRecords="acquisitions.length"
         scrollable
-        scrollHeight="calc(100vh - 350px)"
+        scrollHeight="calc(100vh - 320px)"
         sortField="datetime"
         :sortOrder="-1"
-        :rows="25"
-        paginator
         size="small"
+        selectionMode="multiple"
+        dataKey="filename"
+        :metaKeySelection="true"
+        @rowContextmenu="
+          (event) => {
+            contextMenuRow = event.data
+            event.originalEvent.preventDefault()
+            if (
+              !app.data.acquisition.selected.some(
+                ({ sample_file_id }) => sample_file_id === contextMenuRow.sample_file_id
+              )
+            ) {
+              app.data.acquisition.selected = [contextMenuRow]
+            }
+            contextMenuRef?.show(event.originalEvent)
+          }
+        "
+        :virtualScrollerOptions="{ itemSize: 28 }"
       >
-        <Column selectionMode="multiple" headerStyle="width: 3rem" />
         <Column header="Filename" field="filename" sortable />
         <Column header="Polarity" field="polarity" sortable />
         <Column header="Datetime" field="datetime" sortable />
-        <template #paginatorstart>
-          <strong v-if="app.data.acquisition.multiselected" style="font-style: italic">
-            {{ app.data.acquisition.selected.length }} files selected
-          </strong>
-          <div v-else style="min-width: 11ch" />
-        </template>
-        <template #paginatorend>
-          <div class="info-text">
-            <span v-if="!app.data.batch.focused">
-              <span class="pi pi-info-circle"/>
-              Select a batch to process the files.
-            </span>
-            <span v-else-if="hasBothPolarities">
-              <span class="pi pi-info-circle"/>
-              Cannot process files with both "+" and "-" polarities selected.
-            </span>
-            <span v-else-if="onlyMixedPolaritySelected">
-              <span class="pi pi-info-circle"/>
-              Only mixed polarity files selected. Please choose a polarity from the dropdown.
-            </span>
+        <template #footer>
+          <div
+            style="
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              padding: 0.5rem;
+            "
+          >
+            <strong v-if="app.data.acquisition.multiselected" style="font-style: italic">
+              {{ app.data.acquisition.selected.length }} files selected
+            </strong>
+            <div v-else style="min-width: 11ch" />
+
+            <div class="info-text">
+              <span v-if="!app.data.batch.focused">
+                <span class="pi pi-info-circle" />
+                Select a batch to process the files.
+              </span>
+              <span v-else-if="hasBothPolarities">
+                <span class="pi pi-info-circle" />
+                Cannot process files with both "+" and "-" polarities selected.
+              </span>
+              <span v-else-if="onlyMixedPolaritySelected">
+                <span class="pi pi-info-circle" />
+                Only mixed polarity files selected. Please choose a polarity from the dropdown.
+              </span>
+            </div>
           </div>
         </template>
       </DataTable>
@@ -233,6 +303,7 @@ watchEffect(() => {
         @submit="app.data.acquisition.unfocus()"
       />
       <DialogFileUpload :files="uploadedFiles" />
+      <ContextMenu :model="contextMenuItems" ref="contextMenuRef" />
       <i class="info-line">
         <span class="pi pi-file-arrow-up" /><span>Drag sample files here to upload them</span>
       </i>
@@ -295,6 +366,4 @@ menu :deep(.full) {
 .info-text {
   font-style: italic;
 }
-
-
 </style>
