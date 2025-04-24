6268bf4a5341c691f019e826c0a32a89f44e5540
Philip Chernonog <philip.chernonog@karsa.fi>
2025-04-24 15:48:45 +0300

    Fix `match-lib` overriding the logging configuration for other modules
    loguru configures logging globally, so the last runtime to be initialized should be the one of the module itself. As a kind of hack, we import module's runtime the last, and other modules before

--- server/backend/src/mascope_backend/runtime.py
+++ server/backend/src/mascope_backend/runtime.py
@@ -2,6 +2,7 @@ import mascope_tofwerk.runtime
 import mascope_thermo.runtime
 import mascope_chem.runtime
 import mascope_signal.runtime
+import mascope_match.runtime
 import mascope_file.runtime
 
 
