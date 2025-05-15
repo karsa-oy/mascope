9aaeb3c89591f7afe01d6f00b5e4fc70b65c0247
Matan Bendix Shenhav <matan@shenhav.fyi>
2025-05-15 12:45:47 +0200

    feat(cli): add `env sync` command
    This adds a `mascope env sync` command that uses rsync to allow
    backing, migrating and synchronizing runtime envs locally or remotely.
    
    Destination synlinked directories are maintained to allow mounted
    filestore drives in production environments not to be broken by the
    sync process. A flag to skip the filestore is provided to allow for
    faster transfers.

--- tooling/cli/src/mascope_cli/cmd/env.py
+++ tooling/cli/src/mascope_cli/cmd/env.py
@@ -1,10 +1,13 @@
 import typer
 from typing import Annotated
+import subprocess
+import os
 
 from rich.console import Console
 from rich.table import Table
 
 from mascope_cli.runtime import runtime
+import mascope_cli.cmd.lib as lib
 
 from mascope_runtime import Runtime
 
@@ -72,3 +75,75 @@ def use(
         runtime.logger.info(f"Mascope env set to '{env}'")
     else:
         runtime.logger.error(f"No env named '{env}' was found")
+
+
+@env_app.command()
+def sync(
+    source: Annotated[
+        str,
+        typer.Argument(
+            help="Source environment, optionally in a remote [USER@HOST:]ENV",
+        ),
+    ],
+    destination: Annotated[
+        str,
+        typer.Argument(
+            help="Destination environment, optionally in a remote [USER@HOST:]ENV",
+        ),
+    ],
+    skip_filestore: Annotated[
+        bool | None,
+        typer.Option(
+            "--skip-filestore",
+            "-s",
+            help="Skip synchronizing the filestore",
+        ),
+    ] = False,
+):
+    """
+    Sync a runtime env from source to destination using rsync
+
+    Both source and destination can be local paths or remote locations in an rsync-like format (USER@HOST:ENV).
+    This allows transferring runtime envs between machines or creating backups.
+
+    Note that this can take a while (hours), especially for large environment and across
+    the network. For quicker sync, you can skip the filestore by using the --skip-filestore
+    flag, or -s for short.
+    """
+
+    flags = " ".join(
+        [
+            "--progress",  # show progress bar
+            "--recursive",  # recurse into directories
+            "--copy-links",  # copy symlinks as files
+            "--keep-dirlinks",  # maintain synlinked dirs in destination
+        ]
+        + (
+            ["--exclude 'filestore/*'"]  # skip filestore if specified
+            if skip_filestore
+            else []
+        )
+    )
+
+    def resolve(target: str):
+        if "@" in target:
+            [remote, env] = target.split(":")
+            path = subprocess.run(
+                ["ssh", remote, "bash", "-c", "'echo $MASCOPE_PATH'"],
+                capture_output=True,
+                text=True,
+            ).stdout.strip("\n")
+            return f"{remote}:{path}/.runtime/env/{env}/"
+        else:
+            return runtime.path(".runtime", "env", f"{target}/")
+
+    src = resolve(source)
+    dest = resolve(destination)
+
+    cmd = f"rsync {flags} {src} {dest}"
+
+    runtime.logger.info(
+        f"Syncing {src} -> {dest}" + (" (skipping filestore)" if skip_filestore else "")
+    )
+    runtime.logger.info(cmd)
+    lib.run(cmd)
