b33106aa744cb1f9155f8e889de4e62c0f7708e7
Matan Bendix Shenhav <matan@shenhav.fyi>
2025-07-15 19:04:32 +0300

    feat(frontend): add tabbed panel component

---
+++ server/frontend/src/lib/base/BaseTabbedPanel.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Panel from 'primevue/panel'
+import Button from 'primevue/button'
+import TabMenu from 'primevue/tabmenu'
+
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    required: true
+  },
+  contextMenu: {
+    type: Object,
+    required: false
+  },
+  pt: {
+    type: Object,
+    required: false
+  }
+})
+</script>
+
+<template>
+  <Panel
+    class="browser"
+    style="border: none"
+    @contextmenu.prevent.stop="
+      (event) => {
+        contextMenu?.onClick(event)
+      }
+    "
+    :pt="pt"
+  >
+    <template #header>
+      <TabMenu :model="[{ label, icon }]" />
+    </template>
+    <template #icons>
+      <slot name="menu"></slot>
+    </template>
+    <slot></slot>
+  </Panel>
+</template>
