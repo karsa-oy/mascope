32e9df2882a269eab73e0027f07a294ce601c055
Konstantin Tumashevich <konstantin.tumashevich@karsa.fi>
2025-09-19 15:36:18 +0300

    feat(db): Update v36 migration to pick up calibration flow changes

--- server/backend/src/mascope_backend/db/migration/v36.py
+++ server/backend/src/mascope_backend/db/migration/v36.py
@@ -1,14 +1,26 @@
 """
-Remove cached sum signals.
+Fix Orbi calibration
 """
 
 import asyncio
 import os
 import shutil
+import json
+import glob
+from shutil import rmtree
 
-from mascope_backend.db import configure_database_engine
+from sqlalchemy import select
+
+from mascope_backend.db import async_session, configure_database_engine
 from mascope_backend.db.ops.backup import create_db_backup
-from mascope_backend.db.ops.filestore import delete_sum_signal
+from mascope_backend.db.models import SampleFile, SampleItem
+from mascope_backend.api.controllers.sample.lib.sample_file_compute import (
+    compute_peaks,
+)
+from mascope_backend.api.controllers.match.match_controller import rematch_sample
+
+import mascope_file.name as m_name
+import mascope_file.io as m_io
 
 from mascope_backend.runtime import runtime
 
@@ -27,13 +39,83 @@ async def run():
     shutil.copyfile(old_db_path, new_db_path)
 
     await configure_database_engine(new_version)
-    runtime.logger.info(
-        "Remove cached filtered sum signals, full sum signals will remain"
-    )
 
-    await delete_sum_signal(cached_only=True)
+    # Get the Orbi sample files
+    async with async_session() as session:
+        stmt = select(SampleFile).where(SampleFile.instrument.ilike("%orbi%"))
+        result = await session.execute(stmt)
+        sample_files = result.scalars().all()
+
+    num_of_files = len(sample_files)
+    failed_sample_files_flag = False
+    for i, sample_file in enumerate(sample_files):
+        runtime.logger.info(
+            f"({i+1}/{num_of_files}) Checking file {sample_file.filename}"
+        )
+        try:
+            props = m_io.read_props(sample_file.filename)
+            mz_calibration = props["mz_calibration"]
+            if mz_calibration is None:
+                runtime.logger.info(
+                    f"File {sample_file.filename} has no calibration; skipping."
+                )
+                continue
+
+            runtime.logger.info(
+                "Delete stale sum signals and calibrations and recompute peaks."
+            )
+            _remove_sum_signals(sample_file)
+            await compute_peaks(sample_file.filename, if_exists="replace")
+            updated_props = props.copy()
+            updated_props["mz_calibration"] = None
+            _overwrite_props(sample_file.filename, updated_props)
+
+            runtime.logger.info(f"Rematching samples for file {sample_file.filename}.")
+            await _rematch_samples_by_filename(sample_file.filename)
+
+        except Exception as e:
+            runtime.logger.error(
+                f"Failed to update .props for file {sample_file.filename}: {e}"
+            )
+            failed_sample_files_flag = True
 
     runtime.logger.info(f"Migration to v{new_version} completed.")
+    if failed_sample_files_flag:
+        runtime.logger.warning(
+            "Some sample files failed to update. Please check the logs for details."
+        )
+
+
+def _overwrite_props(base_filename, updated_props):
+    """Overwrite the .props file with updated_props."""
+    sample_data_path = m_name.parse_path_from_item_filename(base_filename)
+    props_path = os.path.join(sample_data_path, ".props")
+    with open(props_path, "w") as f:
+        json.dump(updated_props, f, indent=4)
+
+
+def _remove_sum_signals(sample_file):
+    """Remove all sum_signal* directories in the sample data path."""
+    sample_data_path = m_name.parse_path_from_item_filename(sample_file.filename)
+    pattern = "sum_signal*"
+    sum_signal_dirs = glob.glob(
+        os.path.join(
+            sample_data_path,
+            pattern,
+        )
+    )
+    for zarr_dir in sum_signal_dirs:
+        rmtree(zarr_dir)
+
+
+async def _rematch_samples_by_filename(filename):
+    async with async_session() as session:
+        stmt = select(SampleItem).where(SampleItem.filename == filename)
+        result = await session.execute(stmt)
+        sample_items = result.scalars().all()
+
+    for sample_item in sample_items:
+        await rematch_sample(sample_item.sample_item_id)
 
 
 if __name__ == "__main__":
