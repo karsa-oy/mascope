dc3a34882f236fd79ae899f0e8b2411635e6c4a1
Oskari Kausiala <oskari.kausiala@karsa.fi>
2025-09-23 15:18:51 +0300

    feat(cli): Make `mascope-backend run` command more explicit
    - Show user the list of available services to run
    - Raise an error if the requested service does not exist

--- server/backend/src/mascope_backend/main.py
+++ server/backend/src/mascope_backend/main.py
@@ -12,26 +12,32 @@ def main():
     pass
 
 
+services = ["api-server", "file-converter"]
+
+
 @backend_app.command()
 def run(
     service: Annotated[
-        str | None,
+        str,
         typer.Argument(
-            help="The backend service to launch",
+            help="The backend service to launch, one of: " + ", ".join(services),
         ),
-    ],
+    ] = "api-server",
 ):
     """
     Launch a Mascope backend service
     """
-    if service == "api-server":
-        from mascope_backend.app import run as run_api_server
+    match service:
+        case "api-server":
+            from mascope_backend.app import run as run_api_server
 
-        run_api_server()
-    elif service == "file-converter":
-        from mascope_backend.file_converter.service import run as run_file_converter
+            run_api_server()
+        case "file-converter":
+            from mascope_backend.file_converter.service import run as run_file_converter
 
-        run_file_converter()
+            run_file_converter()
+        case _:
+            raise ValueError(f"Unknown service: {service}")
 
 
 def exec():
