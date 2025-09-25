82c58939606b36eed38eb7135b492c3fa5db24fd
Oskari Kausiala <oskari.kausiala@karsa.fi>
2025-09-25 12:38:12 +0300

    fix(test): Fix "test_calibration_fitting" after removing build_params

--- server/backend/tests/system/workflows/test_calibration_handlers.py
+++ server/backend/tests/system/workflows/test_calibration_handlers.py
@@ -1,14 +1,17 @@
 import os
 import pytest
 import warnings
-from mascope_backend.db import init_db, async_session
+from mascope_backend.db import init_db
 from mascope_backend.api.controllers.calibration.lib.calibration_mz_fit import (
     get_calibration_handler,
 )
 from mascope_backend.api.models.calibration.calibration_pydantic_model import (
     CalibrationFitParams,
 )
-from mascope_backend.db.models import SampleBatch
+from mascope_backend.api.new.ionization.modes.service import get_ionization_mode
+from mascope_backend.api.new.ionization.modes.util import (
+    fetch_sample_ionization_mechanism_ids,
+)
 from utils import get_orbi_raw_files_collection, collect_samples, FakeNotification
 
 
@@ -26,16 +29,12 @@ async def test_calibration_fitting():
     # Orbi raw tests
     orbi_sample_files = await get_orbi_raw_files_collection(samples)
     for filetype, sample in orbi_sample_files.items():
-        async with async_session() as session:
-            sample_batch = await session.get(SampleBatch, sample.sample_batch_id)
-
-        build_params = sample_batch.build_params
-        calibration_mechs = build_params["calibration_ion_mechanisms"]
-        matching_mechs = build_params["ion_mechanisms"]
-        mechanisms = calibration_mechs if calibration_mechs else matching_mechs
-
+        mechanisms = await fetch_sample_ionization_mechanism_ids(sample.sample_item_id)
+        ionization_mode_response = await get_ionization_mode(sample.ionization_mode_id)
+        ionization_mode = ionization_mode_response["data"]
+        calibration_collection_id = ionization_mode["calibration_collection_id"]
         calibration_parameters = CalibrationFitParams(
-            calibration_collection_id=build_params["calibration_collection"],
+            calibration_collection_id=calibration_collection_id,
             ionization_mechanism_ids=mechanisms,
         )
         calibration_handler = get_calibration_handler(
