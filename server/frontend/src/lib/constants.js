3d3266aab01d5f006b7e1edd931bfc8fdc70dee5
Oskari Kausiala <oskari.kausiala@karsa.fi>
2025-10-31 10:34:32 +0200

    fix(ui): Add missing sample type "ACQUISITION"
    Fixes a bug where the sample type was not correctly shown in the sample dialog, in case the type is "ACQUISITION"

--- server/frontend/src/lib/constants.js
+++ server/frontend/src/lib/constants.js
@@ -39,7 +39,7 @@ export const getAllowedCollectionTypes = (batchType) => {
 
 // Sample types by filter ID requirement
 export const sampleTypesFilterIdRequired = ['FILTER_REGENERATION', 'FILTER_BACKGROUND']
-export const sampleTypesFilterIdOptional = ['BLANK', 'SAMPLE', 'UNKNOWN']
+export const sampleTypesFilterIdOptional = ['ACQUISITION', 'BLANK', 'SAMPLE', 'UNKNOWN']
 export const sampleTypesFilterIdNotAllowed = ['INSTRUMENT_BACKGROUND', 'ONLINE']
 
 // Combined sample types for validation
