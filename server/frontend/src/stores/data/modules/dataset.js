1f8c9f5b2b739cd4a826be08728149eb46134b48
Oskari Kausiala <oskari.kausiala@karsa.fi>
2026-05-07 14:26:31 +0300

    fix(ui): Update dataset store RUD operations to use new routes

--- server/frontend/src/stores/data/modules/dataset.js
+++ server/frontend/src/stores/data/modules/dataset.js
@@ -8,6 +8,7 @@ import { useWorkspace } from './workspace'
 export const useDataset = defineStore('app.data.dataset', () => {
   const name = 'dataset'
   const key = 'dataset_id'
+  const path = () => `/workspaces/${useWorkspace().focusedId}/datasets`
 
   const data = useData(
     name,
@@ -27,7 +28,7 @@ export const useDataset = defineStore('app.data.dataset', () => {
         persist: true
       },
       read: (dataset_id) =>
-        api.http.get(`/datasets/${dataset_id}`, {
+        api.http.get(`${path()}/${dataset_id}`, {
           use: 'read',
           type: 'read_dataset'
         })
@@ -38,17 +39,17 @@ export const useDataset = defineStore('app.data.dataset', () => {
     ...data,
     // backend
     create: (dataset) =>
-      api.http.post(`/workspaces/${useWorkspace().focusedId}/datasets`, dataset, {
+      api.http.post(`${path()}`, dataset, {
         use: 'create',
         type: 'create_dataset'
       }),
     update: (dataset) =>
-      api.http.patch(`/datasets/${dataset.dataset_id}`, dataset, {
+      api.http.patch(`${path()}/${dataset.dataset_id}`, dataset, {
         use: 'update',
         type: 'update_dataset'
       }),
     delete: (dataset) =>
-      api.http.delete(`/datasets/${dataset.dataset_id}`, {
+      api.http.delete(`${path()}/${dataset.dataset_id}`, {
         use: 'delete',
         type: 'delete_dataset'
       })
