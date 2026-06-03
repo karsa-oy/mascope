3112623fbf681fea5c81c12f1d7c73f7502eb0b5
Oskari Kausiala <oskari.kausiala@karsa.fi>
2026-06-03 12:19:29 +0300

    fix(ui): Fix potential race condition with target/batch dialogs

--- server/frontend/src/lib/panes/PaneBrowserMatch/MatchCollectionContextMenu.vue
+++ server/frontend/src/lib/panes/PaneBrowserMatch/MatchCollectionContextMenu.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 
 import ContextMenu from 'primevue/contextmenu'
 
@@ -13,11 +13,18 @@ const contextMenuRef = ref()
 onMounted(() => {
   contextMenu.ref = contextMenuRef.value
 })
+
+const collectionOp = computed({
+  get: () => (contextMenu.dialog.op === 'update_targets' ? null : contextMenu.dialog.op),
+  set: (v) => {
+    contextMenu.dialog.op = v
+  }
+})
 </script>
 
 <template>
   <ContextMenu ref="contextMenuRef" :model="contextMenu.entries" @hide="contextMenu.clear" />
-  <DialogTargetCollectionOp v-model:action="contextMenu.dialog.op" />
+  <DialogTargetCollectionOp v-model:action="collectionOp" />
   <DialogBatchOp
     v-model:action="contextMenu.dialog.op"
     :visible="contextMenu.dialog.op === 'update_targets'"
