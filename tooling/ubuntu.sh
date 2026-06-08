af7abbd1cd5d9848c3b7bc503312d723072dfd06
Oskari Kausiala <oskari.kausiala@karsa.fi>
2026-06-08 14:13:29 +0300

    fix(cli): Update Ubuntu install script with deprecated "apt-key"

--- tooling/ubuntu.sh
+++ tooling/ubuntu.sh
@@ -140,10 +140,15 @@ function install_tooling() {
         write_line "Docker not detected, installing..."
        
         sudo apt install -y apt-transport-https ca-certificates software-properties-common
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-        sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-        apt-cache policy docker-ce
-        sudo apt install -y docker-ce
+        sudo install -m 0755 -d /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /tmp/docker.asc
+        sudo cp /tmp/docker.asc /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+        rm /tmp/docker.asc
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" \
+            | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt update
+        sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
         sudo usermod -aG docker "${USER}"
     else
         write_line "Docker detected, skipping install."
